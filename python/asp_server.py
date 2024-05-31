7ef494fa8c8dd7f0867b07d8a3707159fbdeecbe
Yashodha Ravichandran <ravichandranyashodha@gmail.com>
2024-05-31 19:25:34 -0400

    Add server logic

--- python/asp_server.py
+++ python/asp_server.py
@@ -1,12 +1,15 @@
 import asyncio
 from typing import Coroutine,Dict
 import json
-from echo_quic import EchoQuicConnection, QuicStreamEvent
+from asp_quic import ASPQuicConnection, QuicStreamEvent
 import pdu
+import random
 
 
 
-async def echo_server_proto(scope:Dict, conn:EchoQuicConnection):
+
+
+async def asp_server_proto(scope:Dict, conn:ASPQuicConnection):
         
         message:QuicStreamEvent = await conn.receive()
         
@@ -19,5 +22,47 @@ async def echo_server_proto(scope:Dict, conn:EchoQuicConnection):
         dgram_out.mtype |= pdu.MSG_TYPE_DATA_ACK
         dgram_out.msg = "SVR-ACK: " + dgram_out.msg
         rsp_msg = dgram_out.to_bytes()
-        rsp_evnt = QuicStreamEvent(stream_id, rsp_msg, False)
-        await conn.send(rsp_evnt)
+        rsp_vent = QuicStreamEvent(stream_id, rsp_msg, False)
+        await conn.send(rsp_vent)
+
+        # Create an array of data that needs to be streamed 
+        data_stream = ["Hello", "this", "is", "a", "test", "stream"]
+
+        # Initialize stream id 
+        stream_id = None
+
+        # Iterate through the data that needs to be streamed
+
+        for i in range(len(data_stream)):
+            
+            # Create a pdu for each the current message
+            if i != (len(data_stream) - 1):
+                dgram_out = pdu.Datagram(pdu.MSG_TYPE_DATA, data_stream[i], i, False)
+            else:
+                dgram_out = pdu.Datagram(pdu.MSG_TYPE_DATA, data_stream[i], i, True)
+
+            # Convert the pdu into bytes to be streamed
+            rsp_msg = dgram_out.to_bytes()
+
+            if stream_id is None:
+                # Open a new stream
+                rsp_event = await conn.new_stream(rsp_msg)
+                stream_id = rsp_event.stream_id
+            else:
+                # Use the existing stream
+                rsp_event = QuicStreamEvent(stream_id, rsp_msg, False)
+                await conn.send(rsp_vent)
+
+
+        # Wait for the final acknowledgment message from the client
+        final_msg: QuicStreamEvent = await conn.receive()
+        final_dgram_in = pdu.Datagram.from_bytes(final_msg.data)
+        print("[svr] received final ack: ", final_dgram_in.msg)
+
+
+
+
+
+
+
+
